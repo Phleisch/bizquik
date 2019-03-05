65eb3643c753e293206a4c4de0c3b0e506e02f50
phleisch <KaiFleischmans@gmail.com>
2019-03-05 06:31:16 +0000

    Created user object, started with instance variables

--- user_dao.py
+++ user_dao.py
@@ -1,1 +1,14 @@
 # Database Access Object for the users table of bizquik's database
+from sqlalchemy import Column, DateTime, Integer, String
+
+class User(db):
+    __tablename__ = 'users'
+
+    id            = Column(Integer, primary_key=True)
+    username      = Column(String)
+    password_salt = Column(String)
+    password      = Column(String)
+    first_name    = Column(String)
+    last_name     = Column(String)
+    email         = Column(String)
+    date_created  = Column(DateTime)
