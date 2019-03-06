ccd88b252c2e3b257b022f35c285174985d80da0
phleisch <KaiFleischmans@gmail.com>
2019-03-06 05:21:59 +0000

    Added __init__ to user_dao

--- user_dao.py
+++ user_dao.py
@@ -4,16 +4,24 @@ from sqlalchemy import Column, DateTime, Integer, String
 class User(db):
     __tablename__ = 'users'
 
-    id            = Column(Integer, primary_key=True)
-    username      = Column(String)
-    password_salt = Column(String)
-    password      = Column(String)
-    first_name    = Column(String)
-    last_name     = Column(String)
-    email         = Column(String)
-    date_created  = Column(DateTime)
+    id              = Column(Integer, primary_key=True)
+    username        = Column(String(100))
+    password_salt   = Column(String(100))
+    password        = Column(String(100))
+    first_name      = Column(String(100))
+    last_name       = Column(String(100))
+    email           = Column(String(255))
+    date_created    = Column(DateTime)
+
+    def __init__(self, username, password_raw, first_name, last_name, email):
+        self.username       = username
+        self.password_salt  = generate_salt()
+        self.password       = generate_hash(password_raw, password_salt)
+        self.first_name     = first_name
+        self.last_name      = last_name
+        self.email          = email
 
     # String representation of this object
     def __repr__(self):
         return "<User(username = '%s', full name = '%s %s', email = '%s')>" \
-        % (self.username, self.first_name, self.last_name, self.email)
+            % (self.username, self.first_name, self.last_name, self.email)
